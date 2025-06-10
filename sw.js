1eed403f1c0340f6b50aa8051a410f37496ce234
man25r18 <faericcardo0@gmail.com>
2025-06-10 13:24:42 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,37 +1,22 @@
-const CACHE_NAME = 'calculator-v1';
+const CACHE_NAME = 'calcolatrice-v1';
 const urlsToCache = [
   '/',
-  '/index.html',
-  '/manifest.json',
-  '/icon-192.png',
-  '/icon-512.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/react/18.2.0/umd/react.production.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/react-dom/18.2.0/umd/react-dom.production.min.js',
-  'https://cdnjs.cloudflare.com/ajax/libs/babel-standalone/7.22.5/babel.min.js',
-  'https://cdn.tailwindcss.com'
+  '/static/js/bundle.js',
+  '/static/css/main.css',
+  '/manifest.json'
 ];
 
-// Install
-self.addEventListener('install', event => {
+self.addEventListener('install', (event) => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then(cache => {
-        return Promise.all(
-          urlsToCache.map(url => {
-            return cache.add(url).catch(err => {
-              console.log('Failed to cache:', url, err);
-            });
-          })
-        );
-      })
+      .then((cache) => cache.addAll(urlsToCache))
   );
 });
 
-// Fetch
-self.addEventListener('fetch', event => {
+self.addEventListener('fetch', (event) => {
   event.respondWith(
     caches.match(event.request)
-      .then(response => {
+      .then((response) => {
         // Cache hit - return response
         if (response) {
           return response;
@@ -39,20 +24,5 @@ self.addEventListener('fetch', event => {
         return fetch(event.request);
       }
     )
-  );
-});
-
-// Activate
-self.addEventListener('activate', event => {
-  event.waitUntil(
-    caches.keys().then(cacheNames => {
-      return Promise.all(
-        cacheNames.map(cacheName => {
-          if (cacheName !== CACHE_NAME) {
-            return caches.delete(cacheName);
-          }
-        })
-      );
-    })
   );
 });

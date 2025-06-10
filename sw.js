f3f37b4d413f1c32570c3fc8a226485e11462a04
man25r18 <faericcardo0@gmail.com>
2025-06-10 16:11:58 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,28 +1,78 @@
-const CACHE_NAME = 'calcolatrice-v1';
+const CACHE_NAME = 'calc-pwa-v1';
 const urlsToCache = [
   '/',
-  '/static/js/bundle.js',
-  '/static/css/main.css',
-  '/manifest.json'
+  '/index.html',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png',
+  'https://cdn.tailwindcss.com',
+  'https://unpkg.com/react@18/umd/react.production.min.js',
+  'https://unpkg.com/react-dom@18/umd/react-dom.production.min.js',
+  'https://unpkg.com/@babel/standalone/babel.min.js'
 ];
 
-self.addEventListener('install', (event) => {
+// Installazione del Service Worker
+self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
-      .then((cache) => cache.addAll(urlsToCache))
+      .then(cache => {
+        console.log('Cache aperta');
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+// Gestione delle richieste
+self.addEventListener('fetch', event => {
   event.respondWith(
     caches.match(event.request)
-      .then((response) => {
-        // Cache hit - return response
+      .then(response => {
+        // Restituisce il file dalla cache se disponibile
         if (response) {
           return response;
         }
-        return fetch(event.request);
+        
+        // Altrimenti fa la richiesta alla rete
+        return fetch(event.request).then(response => {
+          // Controlla se la risposta è valida
+          if (!response || response.status !== 200 || response.type !== 'basic') {
+            return response;
+          }
+
+          // Clona la risposta
+          const responseToCache = response.clone();
+
+          caches.open(CACHE_NAME)
+            .then(cache => {
+              cache.put(event.request, responseToCache);
+            });
+
+          return response;
+        });
       }
     )
   );
+});
+
+// Aggiornamento del Service Worker
+self.addEventListener('activate', event => {
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheName !== CACHE_NAME) {
+            console.log('Eliminazione cache vecchia:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+// Gestione messaggi dal client
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
